d16edf4522428237dcb3fac0f6b2c8d2c41bd1de
Jake <jhughes@ms3-inc.com>
2020-11-23 10:17:23 -0500

    Updated DefaultJavaFormatPlugin to remove RecentsMap

--- src/main/java/com/datasonnet/plugins/DefaultJavaFormatPlugin.java
+++ src/main/java/com/datasonnet/plugins/DefaultJavaFormatPlugin.java
@@ -16,7 +16,6 @@ package com.datasonnet.plugins;
  * limitations under the License.
  */
 
-import com.datasonnet.RecentsMap;
 import com.datasonnet.document.DefaultDocument;
 import com.datasonnet.document.Document;
 import com.datasonnet.document.MediaType;
@@ -37,6 +36,7 @@ import ujson.Value;
 import javax.xml.bind.JAXBElement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TimeZone;
 
@@ -48,7 +48,7 @@ public class DefaultJavaFormatPlugin extends BaseJacksonDataFormatPlugin {
     public static final String DS_PARAM_TYPE = "type";  // aligns with existing java object mimetypes
     public static final String DS_PARAM_OUTPUT_CLASS = "outputclass";  // supports legacy
 
-    private static final Map<Integer, ObjectMapper> MAPPER_CACHE = new RecentsMap<>(64);
+    private static final Map<Integer, ObjectMapper> MAPPER_CACHE = new LinkedHashMap<>(16,.9f);
 
     static {
         SimpleModule module = new SimpleModule();
